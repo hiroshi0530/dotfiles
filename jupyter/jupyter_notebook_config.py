b98e56622e3c97c116f2d7e279f546de602a2681
hiroshi0530 <whiroshi0530@gmail.com>
2020-02-14 20:02:03 +0900

    jupyter notebookをgit管理を演算の順番や出力結果を対象にしない

--- jupyter/jupyter_notebook_config.py
+++ jupyter/jupyter_notebook_config.py
@@ -1,2 +1,62 @@
-c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
-c.ContentsManager.default_jupytext_formats = "ipynb,py"
+# 200214現在、古い（削除しても良い）
+# c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
+# c.ContentsManager.default_jupytext_formats = "ipynb,py"
+
+# 
+# 200214: 
+# 
+# ipynbの更新時に、pyの方も自動で更新
+# その際、出力結果や演算の順番も削除
+# 
+# https://qiita.com/mmsstt/items/6f8382afcc94f57861d4
+# http://jupyter-notebook.readthedocs.org/en/latest/extending/savehooks.html
+# 
+
+def scrub_output_pre_save(model, **kwargs):
+    """scrub output before saving notebooks"""
+    # only run on notebooks
+    if model['type'] != 'notebook':
+        return
+    # only run on nbformat v4
+    if model['content']['nbformat'] != 4:
+        return
+
+    for cell in model['content']['cells']:
+        if cell['cell_type'] != 'code':
+            continue
+        cell['outputs'] = []
+        cell['execution_count'] = None
+
+c.FileContentsManager.pre_save_hook = scrub_output_pre_save
+
+
+import io
+import os
+from notebook.utils import to_api_path
+
+_script_exporter = None
+
+def script_post_save(model, os_path, contents_manager, **kwargs):
+    """convert notebooks to Python script after save with nbconvert
+
+    replaces `ipython notebook --script`
+    """
+    from nbconvert.exporters.script import ScriptExporter
+
+    if model['type'] != 'notebook':
+        return
+
+    global _script_exporter
+    if _script_exporter is None:
+        _script_exporter = ScriptExporter(parent=contents_manager)
+    log = contents_manager.log
+
+    base, ext = os.path.splitext(os_path)
+    py_fname = base + '.py'
+    script, resources = _script_exporter.from_filename(os_path)
+    script_fname = base + resources.get('output_extension', '.txt')
+    log.info("Saving script /%s", to_api_path(script_fname, contents_manager.root_dir))
+    with io.open(script_fname, 'w', encoding='utf-8') as f:
+        f.write(script)
+
+c.FileContentsManager.post_save_hook = script_post_save
