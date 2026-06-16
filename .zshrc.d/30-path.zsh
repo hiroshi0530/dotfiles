e395723720eda3981e7f19d4504efa9fb41c0bdc
Hiroshi Wayama <hiroshi.wayama@gmail.com>
2026-06-16 09:52:05 +0900

    refactor(path): anaconda3/poetry/cargo のコメントアウト行を完全削除

--- .zshrc.d/30-path.zsh
+++ .zshrc.d/30-path.zsh
@@ -14,17 +14,11 @@ path=(
 
 [[ -d /opt/local/bin ]]                         && path+=(/opt/local/bin)
 [[ -d /opt/local/sbin ]]                        && path+=(/opt/local/sbin)
-# anaconda3: mise Python と競合するため削除
-# [[ -d "$HOME/anaconda3/bin" ]] && path+=("$HOME/anaconda3/bin")
 [[ -d "$HOME/go/bin" ]]                         && path+=("$HOME/go/bin")  # go install 先
 [[ -d "$HOME/go/ugo/bin" ]]                     && path+=("$HOME/go/ugo/bin")
 [[ -d "$HOME/go/go_test/bin" ]]                 && path+=("$HOME/go/go_test/bin")
 [[ -d "$HOME/.local/bin" ]]                     && path+=("$HOME/.local/bin")
-# poetry: mise 管理に移行したため削除
-# [[ -d /opt/poetry/bin ]] && path+=(/opt/poetry/bin)
 [[ -d "$HOME/.pulumi/bin" ]]                    && path+=("$HOME/.pulumi/bin")
-# cargo/bin: mise rust 管理に移行したため削除
-# [[ -d "$HOME/.cargo/bin" ]] && path+=("$HOME/.cargo/bin")
 [[ -d "$HOME/.antigravity/antigravity/bin" ]]   && path+=("$HOME/.antigravity/antigravity/bin")
 [[ -d "$HOME/win32yank/win32yank-x64" ]]        && path+=("$HOME/win32yank/win32yank-x64")
 
