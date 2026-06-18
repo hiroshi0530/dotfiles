5b56e2b7fcd9383018355f98236796a7afd197b1
Hiroshi Wayama <hiroshi0530@users.noreply.github.com>
2026-06-18 12:05:11 +0900

    chore: remove eza, confirm ripgrep in mise managed tools
    - mise: remove eza (ripgrep was already managed)
    - aliases: revert ll/la/l/tree/ftree to ls/tree (remove eza conditional)
    - 70-tools.zsh: remove eza branch from FZF_ALT_C_OPTS

--- .zshrc.d/70-tools.zsh
+++ .zshrc.d/70-tools.zsh
@@ -14,12 +14,7 @@ if [[ -f /opt/homebrew/opt/fzf/shell/key-bindings.zsh ]]; then
 elif [[ -f /usr/share/fzf/key-bindings.zsh ]]; then
   source /usr/share/fzf/key-bindings.zsh
 fi
-# eza を使う場合はツリー表示に eza を利用する
-if command -v eza >/dev/null 2>&1; then
-  export FZF_ALT_C_OPTS="--preview 'eza --tree --icons=never {} | head -200'"
-else
-  export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
-fi
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 
 # zoxide (modern autojump)
 # z <dir> で移動、zi で fzf 対話選択
